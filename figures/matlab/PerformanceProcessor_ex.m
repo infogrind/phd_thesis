7e9369e99111878bd58e304aec1d429978422524
Marius Kleiner <kleiner@gmail.com>
2010-02-27 16:06:11 +0000

    - Added new manualmacros package to list of included packages. - property and method descriptions in simulator chapter now use the macros from   manualmacros.sty. - Revised first two sections of simulator chapter (up to but not including   reference section). - Added chapter label in ch_mindelbwex.tex. - Updated PerformanceProcessor_ex.m to use the modified PerformanceProcessor.
    git-svn-id: svn+ssh://dionysos/volume1/homes/marius/svnroot/epfldocs/work@639 2b8fe21e-8702-0410-8809-fff53b49c4e2

--- figures/matlab/PerformanceProcessor_ex.m
+++ figures/matlab/PerformanceProcessor_ex.m
@@ -1,33 +1,28 @@
 %%% MK:STARTSHOW
 classdef PerformanceProcessor < SchemeProcessor
-
+    
     properties
         mse
     end
-    
-    methods (Access = 'public')
-        function plot_performance(obj, schemes, parameters)
-            set_schemes(obj, schemes, parameters);
-            obj.mse = zeros(nb_schemes(obj), length(obj.snr));
-            process_schemes(obj);
-            perf_plot(obj);
-        end
-    end
-    
+
     methods (Access = 'protected')
-        function post_process(obj, scheme, j, k)
+        function initialize(obj)
+            obj.mse = zeros(nb_schemes(obj), length(obj.snr));
+        end
+        
+        function save_scheme_data(obj, scheme, j, k)
             obj.mse(k, j) = scheme.compute_mse();
         end
-
-        function perf_plot(obj)
-            % ...
-%%% MK:ENDSHOW
+        
+        function post_process(obj)
+          % Create the performance plot.
+          % ...
+        %%% MK:ENDSHOW
             obj.om.ylabel = 'SDR [dB]';
             obj.om.plottitle = 'SDR vs. SNR';
-            plot_vs_csnr_db(obj, obj.sv ./ obj.mse);
-%%% MK:STARTSHOW
+            plot_vs_snr_db(obj, obj.sv ./ obj.mse);
+        %%% MK:STARTSHOW
         end
     end
-    
 end
 %%% MK:ENDSHOW
