877bf6aec797a854bfd445324fca9964dcefca40
Marius Kleiner <kleiner@gmail.com>
2010-03-04 18:00:27 +0000

    Added an scheme where SNR^\epsilon is computed by solving exactly the achievability lemmata.
    git-svn-id: svn+ssh://dionysos/volume1/homes/marius/svnroot/epfldocs/work@670 2b8fe21e-8702-0410-8809-fff53b49c4e2

---
+++ simulator/@AltLambertScalarHybridScheme/AltLambertScalarHybridScheme.m
@@ -0,0 +1,43 @@
+classdef AltLambertScalarHybridScheme < ScalarHybridScheme
+    %ALTLAMBERTSCALARHYBRIDSCHEME SNR^\epsilon computed from achievability bds.
+    %   Detailed explanation goes here
+    
+    properties
+        c1
+        c2
+    end
+    
+    methods (Access = 'public')
+        function obj = AltLambertScalarHybridScheme(sv, s, p, c1, c2)
+            % For this scheme, m = 1 and p is arbitrary.
+            % We can pass an arbitrary value for e (here 1) since this class
+            % will compute it dynamically as a function of the SNR.
+            obj@ScalarHybridScheme(sv, s, p, 1);
+            obj.c1 = c1;
+            
+            % Default value for c2: 8
+            if nargin < 5
+                obj.c2 = 8;
+            else
+                obj.c2 = c2;
+            end
+        end
+        
+        
+        function b = compute_beta(obj)
+            % Shortcuts for n, c, and snr to clear up the following code.
+            n = obj.n;
+            snr = obj.snr;
+            c1 = obj.c1;
+            c2 = obj.c2;
+            
+            % This is the solution of SNR^\epsilon from the lower bound theorem.
+            SNRe = c1 * (n-1) * ...
+                lambertw(snr^(n / (n-1)) / ( c2*(n-1) ));
+            
+            % Set beta as a function of SNR^\epsilon.
+            b = ceil(sqrt(snr / SNRe));
+        end        
+    end    
+end
+
