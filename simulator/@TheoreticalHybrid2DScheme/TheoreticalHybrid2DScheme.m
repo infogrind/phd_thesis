c5df820fe82f2b2f170d58c5424f180b17aad473
Marius Kleiner <kleiner@gmail.com>
2010-03-05 12:44:29 +0000

    - Made TheoreticalHybrid2DScheme more accurate, based on theoretical   calculations. - Practical scheme now also displays difference between theoretical and   empirical SNR.
    git-svn-id: svn+ssh://dionysos/volume1/homes/marius/svnroot/epfldocs/work@673 2b8fe21e-8702-0410-8809-fff53b49c4e2

--- simulator/@TheoreticalHybrid2DScheme/TheoreticalHybrid2DScheme.m
+++ simulator/@TheoreticalHybrid2DScheme/TheoreticalHybrid2DScheme.m
@@ -16,7 +16,29 @@ classdef TheoreticalHybrid2DScheme < TheoreticalScheme
     
     methods (Access = 'protected')
         function update_mse(obj)
-            obj.mse = obj.snr^-(obj.n - (obj.n-1)*obj.epsilon);
+            obj.mse = mse_q(obj) + mse_e(obj);
+        end
+        
+        function e = mse_q(obj)
+            g = 1; % Gamma = 1
+            b = compute_beta(obj);
+            snr = obj.snr;
+            
+            e = .5 * exp(-snr / (8*g^2*b^2)) * ...
+                (3*g*b/sqrt(2*pi*snr) + g^2*b^2/snr + .25);
+        end
+        
+        function e = mse_e(obj)
+            se = 1/12; % sigma_e, here the error is assumed uniform
+            snr = obj.snr;
+            b = compute_beta(obj);
+            n = obj.n; %#ok<*PROP>
+            
+            e = se / ( (1 + snr) * b^(2*(n-1)) );
+        end
+        
+        function b = compute_beta(obj)
+            b = ceil(obj.snr^((1 - obj.epsilon)/2));
         end
     end
     
