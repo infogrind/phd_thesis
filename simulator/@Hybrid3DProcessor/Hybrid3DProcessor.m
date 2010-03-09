866a84db8c56de743bb3aa010fa94da445247d73
Marius Kleiner <kleiner@gmail.com>
2010-03-09 14:55:53 +0000

    A special scheme processor for hybrid schemes with n = 3 that computes the errors in Q_1 and Q_2 separately. It does not plot anything by itself, the data has to be extracted and plotted with a separate output module.
    git-svn-id: svn+ssh://dionysos/volume1/homes/marius/svnroot/epfldocs/work@684 2b8fe21e-8702-0410-8809-fff53b49c4e2

---
+++ simulator/@Hybrid3DProcessor/Hybrid3DProcessor.m
@@ -0,0 +1,59 @@
+classdef Hybrid3DProcessor < PracticalPerformanceProcessor
+    %HYBRID3DPROCESSOR Computes Eq and Ee for 3 ch. uses / source symbol
+    %   This processor is for hybrid schemes with m = 1 and n = p = 3.
+    
+    properties (Access = 'public')
+        q1e
+        q2e
+        ee
+    end
+    
+    methods (Access = 'protected')
+        function initialize(obj)
+            initialize@PracticalPerformanceProcessor(obj);
+            obj.q1e = zeros(nb_schemes(obj), length(obj.snr));
+            obj.q2e = zeros(nb_schemes(obj), length(obj.snr));
+            obj.ee  = zeros(nb_schemes(obj), length(obj.snr));
+        end
+        
+        function save_scheme_data(obj, scheme, j, k)
+            save_scheme_data@PracticalPerformanceProcessor(obj, scheme, j, k);
+            % Get all needed data from scheme object.
+            q  = scheme.compute_q();
+            q1 = q(1,:);
+            q2 = q(2,:);
+            
+            qh = scheme.compute_qh();
+            q1h = qh(1,:);
+            q2h = qh(2,:);
+            
+            e  = scheme.compute_e();
+            eh = scheme.compute_eh();
+            b  = scheme.compute_beta();
+            
+            % Check validity of assumptions.
+            assert(size(q,1) == 2);
+            assert(size(qh, 1) == 2);
+            assert(size(e, 1) == 1);
+            assert(size(eh, 1) == 1);
+            
+            obj.q1e(j, k) = mean((q1 - q1h).^2);
+            obj.q2e(j, k) = mean((q2 - q2h).^2);
+            obj.ee(j, k)  = mean((e - eh).^2) / b^2;
+            
+            if (obj.q1e(j,k) < 1e-9)
+                fprintf('*** Good case: err = %.2e\n', obj.q1e(j,k));
+            else
+                fprintf('*** Bad case:  err = %.2e\n', obj.q1e(j,k));
+                fprintf('*** Number of errors: %d\n', ...
+                    nnz((q1 - q1h).^2 ~= 0));
+            end
+        end
+        
+        function post_process(obj) %#ok<MANU>
+            % Does nothing.
+        end
+    end
+    
+end
+
