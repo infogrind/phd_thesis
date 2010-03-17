d57b59c3c719aca4a0ea10f78c14a109f4db3d49
Marius Kleiner <kleiner@gmail.com>
2010-03-17 15:57:28 +0000

    - Started implementation overview section in simulator chapter. - Added MK:STARTSHOW and MK:ENDSHOW tags to the Scheme.m code and added it to   the list of files in the Makefile from which .minc files must be generated,   because it is now included in the chapter.
    git-svn-id: svn+ssh://dionysos/volume1/homes/marius/svnroot/epfldocs/work@690 2b8fe21e-8702-0410-8809-fff53b49c4e2

---
+++ figures/matlab/Scheme.m
@@ -0,0 +1,1 @@
+../../simulator/@Scheme/Scheme.m
--- simulator/@Scheme/Scheme.m
+++ simulator/@Scheme/Scheme.m
@@ -1,4 +1,4 @@
-classdef Scheme < handle
+classdef Scheme < handle % MK:SHOW
     %SCHEME Base class for all communication schemes.
     %   Any communication scheme inherits from this base class. If a scheme is
     %   practical rather than just theoretical, it should extend
@@ -12,51 +12,51 @@ classdef Scheme < handle
     end
 
     
-    
+    %%% MK:STARTSHOW
     properties (Access = 'protected')
-        % Source variance
-        sv
-        
-        % Channel SNR (power per channel input divided by noise variance)
-        snr
-        
-        % The MSE.
-        mse
+        sv    % Source variance.
+        snr   % SNR (power per channel input symbol).
+        mse   % Mean squared error resulting from the simulation.
     end
     
-    
-    
-    methods (Access = 'public')
+    %%% MK:ENDSHOW
+
+ 
+    methods (Access = 'public') % MK:SHOW
         % The constructor takes two values: The source variance and the source
         % symbols. Note that this base class ignores the source symbols; only
         % classes derived from PracticalScheme will store s. 
-        function obj = Scheme(sv, s) %#ok<INUSD>
+        %%% MK:STARTSHOW
+        function obj = Scheme(sv, s)
             obj.sv = sv;
         end
         
+        %%% MK:ENDSHOW
         
         % When the SNR is changed, the abstract function snr_updated() is called
         % so that derived classes can update the simulation results.
+        %%% MK:STARTSHOW
         function set_snr(obj, snr)
             obj.snr = snr;
             snr_updated(obj);
         end
+
+        %%% MK:ENDSHOW
         
         
         % Every scheme must at least return the MSE.
+        %%% MK:STARTSHOW
         function mse = compute_mse(obj)
             mse = obj.mse;
         end
     end
     
+    %%% MK:ENDSHOW
     
-    
-    methods (Access = 'protected', Abstract = true)
+    methods (Access = 'protected', Abstract = true) % MK:SHOW
         % This function is called each time the SNR is changed to a different
         % value.
-        snr_updated(obj)
-        
-    end
-    
-end
+        snr_updated(obj) % MK:SHOW
+    end % MK:SHOW
+end % MK:SHOW
 
